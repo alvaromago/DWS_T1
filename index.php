3e16e77e570ea87bb9fd898449f1b0038895befc
alvaromago <aalvaromago@gmail.com>
2023-10-02 12:13:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8" />
+	<title>Ejemplo PHP</title>
+</head>
+
+<body>
+	<h1>Ejemplo PHP</h1>
+	<p>Este es un ejemplo de página con código PHP.</p>
+	<?php
+	// Comentario de una línea
+	# También comentario de una línea
+	/* Comentario de varias lineas */
+	echo "<p>Esto es un <b>texto escrito</b> en PHP.</p>";
+	?>
+</body>
+
+</html>
